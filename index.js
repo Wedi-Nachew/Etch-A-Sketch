4dcb7b346826b147489580e5b0d3f708ecf61acf
Filomn Abrha <filmonabrha2020@gmail.com>
2023-06-13 17:15:38 +0300

    Randomize the square's color when hovered

--- index.js
+++ index.js
@@ -1,21 +1,22 @@
 const container = document.querySelector("#container");
 const btn = document.querySelector("#ask");
-let red = Math.floor(Math.random() * 257)
-let green = Math.floor(Math.random() * 257)
-let blue = Math.floor(Math.random() * 257)
-
 let  grids = (num=16) => {
     for(let i=1; i <= (num**2); i++){
         const div = document.createElement("div")
         container.appendChild(div).classList.add("grid-item")
-        div.style.cssText = `background-color: yellow; height:${(102400/(num**2))**(1/2)}px; width: ${(102400/(num**2))**(1/2)}px` 
+        div.style.cssText = `height:${(102400/(num**2))**(1/2)}px; width: ${(102400/(num**2))**(1/2)}px` 
    }
 }
 grids()
 let gridItem = document.querySelectorAll(".grid-item")
 gridItem.forEach(item => item.addEventListener("mouseover", 
 function() {
-    item.style.backgroundColor = `rgb(${red},${green},${blue})`;
+    for(let i=1; i< gridItem.length; i++){
+        const red = Math.floor(Math.random() * 257)
+        const green = Math.floor(Math.random() * 257)
+        const blue = Math.floor(Math.random() * 257)
+        item.style.backgroundColor = `rgb(${red},${green},${blue})`;
+    }
 }))
 btn.textContent = "Adject number of grids"
 btn.addEventListener("click", function() {
