2f42da8113c55da988665690d20e4e09c57fb3ac
Filomn Abrha <filmonabrha2020@gmail.com>
2023-06-13 11:40:35 +0300

    Make the mouse leave a trail

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const btn = document.querySelector("#ask");
 btn.textContent = "Adject number of grids"
 btn.addEventListener("click", function() {
     let numofGrids = prompt("how many columns and rows do you want to create")
+    grids(numofGrids, numofGrids)
 })
 
 function grids(rows, columns) {
@@ -11,12 +12,13 @@ function grids(rows, columns) {
         container.appendChild(div).classList.add("grid-item")
    }
 }
+grids(16,16)
 const gridItem = document.querySelectorAll(".grid-item")
 gridItem.forEach(item => item.addEventListener("mouseover", 
 function() {
     item.style.backgroundColor = "blue"
 }))
-gridItem.forEach(item => item.addEventListener("mouseout", 
-function() {
-    item.style.backgroundColor = ""
-}))
+// gridItem.forEach(item => item.addEventListener("mouseout", 
+// function() {
+//     item.style.backgroundColor = ""
+// }))
