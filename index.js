28ba417e022092e4c2287f41338ec9bcb3981885
Filomn Abrha <filmonabrha2020@gmail.com>
2023-06-13 11:15:09 +0300

    Add a text content to the button

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 const container = document.querySelector("#container");
 const btn = document.querySelector("#ask");
+btn.textContent = "Adject number of grids"
 btn.addEventListener("click", function() {
-    let n = prompt("how many columns and rows do you want to create")
+    let numofGrids = prompt("how many columns and rows do you want to create")
 })
 
-function grids() {
-    for(let i=1; i <= (16*16); i++){
+function grids(rows, columns) {
+    for(let i=1; i <= (rows*columns); i++){
         const div = document.createElement("div")
         container.appendChild(div).classList.add("grid-item")
    }
 }
-grids()
 const gridItem = document.querySelectorAll(".grid-item")
 gridItem.forEach(item => item.addEventListener("mouseover", 
 function() {
