90dea8ffd453b3b6ecbac5d5b3ebcbc2c20c0ff2
Filomn Abrha <filmonabrha2020@gmail.com>
2023-07-09 00:16:45 +0300

    Enable the options to toggle or remove the lines

--- index.js
+++ index.js
@@ -7,83 +7,96 @@ const green = document.querySelector("#green")
 const multiColor = document.querySelector("#multi-color")
 const gridBorder = document.querySelector("#grid-border")
 const output = document.getElementById("output");
+let linesCount = 0;
 
 
-let  grids = (num=16) => {
-    let size = Math.sqrt(parseFloat(160000/(num**2)))
-    for(let i=1; i <= (num ** 2); i++){
+
+let grids = (num = 16) => {
+    let size = Math.sqrt(parseFloat(160000 / (num ** 2)))
+    for (let i = 1; i <= (num ** 2); i++) {
         const div = document.createElement("div")
         container.appendChild(div).classList.add("grid-item")
         div.style.cssText = `height:${size}px; 
-        width: ${size}px` 
-        container.setAttribute(`style`, `grid: 1fr / repeat(${num}, 1fr)`)
-   }
+        width: ${size}px;`
+        container.setAttribute(`style`, `grid: 1fr / repeat(${num}, 1fr)`);
+        (linesCount) ? div.style.cssText += "border: 1px solid gray" : div.style.cssText += "border: none"
+    }
 }
 grids()
-
 
 let gridItem = document.querySelectorAll(".grid-item")
 gridItem.forEach(item => item.addEventListener("mouseover", colors))
 
-btn.addEventListener("input", function() {
-
-    while(container.firstChild){
-         container.removeChild(container.firstChild)
+btn.addEventListener("input", () => {
+    while (container.firstChild) {
+        container.removeChild(container.firstChild)
     }
     grids(btn.value);
     output.value = btn.value + " x " + btn.value;
     gridItem = document.querySelectorAll(".grid-item")
-    gridItem.forEach(item => item.addEventListener("mouseover",()=>{
+    gridItem.forEach(item => item.addEventListener("mouseover", () => {
         colors();
     }))
 })
+gridBorder.addEventListener("click", () => {
+    if (!linesCount) {
+        gridItem.forEach(item => item.style.border = "1px solid gray");
+        gridBorder.textContent = "Remove Lines";
+        linesCount++;
+    } else if (linesCount) {
+        gridItem.forEach(item => item.style.border = "none");
+        gridBorder.textContent = "Toggle Lines"
+        linesCount = 0;
+    }
+})
 
-function colors(WhichOne = "black"){
-    if(WhichOne== "red") {
-        gridItem.forEach(item => item.addEventListener("mouseover", 
-            function() {
-    item.style.backgroundColor = "red";
+function colors(WhichOne = "black") {
+    if (WhichOne == "red") {
+        gridItem.forEach(item => item.addEventListener("mouseover",
+            function () {
+                item.style.backgroundColor = "red";
             }))
-    } else if (WhichOne== "white") {
-        gridItem.forEach(item => item.addEventListener("mouseover", 
-            function() {
-    item.style.backgroundColor = "white";
+    } else if (WhichOne == "white") {
+        gridItem.forEach(item => item.addEventListener("mouseover",
+            function () {
+                item.style.backgroundColor = "white";
             }))
-    } else if (WhichOne== "green") {
-        gridItem.forEach(item => item.addEventListener("mouseover", 
-            function() {
-    item.style.backgroundColor = "green";
+    } else if (WhichOne == "green") {
+        gridItem.forEach(item => item.addEventListener("mouseover",
+            function () {
+                item.style.backgroundColor = "green";
             }))
-    } else if (WhichOne== "black") {
-        gridItem.forEach(item => item.addEventListener("mouseover", 
-            function() {
-    item.style.backgroundColor = "black";
+    } else if (WhichOne == "black") {
+        gridItem.forEach(item => item.addEventListener("mouseover",
+            function () {
+                item.style.backgroundColor = "black";
             }))
-    } else if (WhichOne== "multi-color") {
-        gridItem.forEach(item => item.addEventListener("mouseover", 
-        function() {for(let i=1; i< gridItem.length; i++){
-            const red = Math.floor(Math.random() * 256)
-            const green = Math.floor(Math.random() * 256)
-            const blue = Math.floor(Math.random() * 256)
-            item.style.backgroundColor = `rgb(${red},${green},${blue})`;
-        }
-    })) 
+    } else if (WhichOne == "multi-color") {
+        gridItem.forEach(item => item.addEventListener("mouseover",
+            function () {
+                for (let i = 1; i < gridItem.length; i++) {
+                    const red = Math.floor(Math.random() * 256)
+                    const green = Math.floor(Math.random() * 256)
+                    const blue = Math.floor(Math.random() * 256)
+                    item.style.backgroundColor = `rgb(${red},${green},${blue})`;
+                }
+            }))
     }
 }
-function backgroundColor(option){
-    if(option =="red-bkg") {
+function backgroundColor(option) {
+    if (option == "red-bkg") {
         container.style.backgroundColor = "red"
     } else if (option == "white-bkg") {
         container.style.backgroundColor = "white"
-    } else if(option =="black-bkg"){
+    } else if (option == "black-bkg") {
         container.style.backgroundColor = "black"
-    } else if(option == "green-bkg"){
+    } else if (option == "green-bkg") {
         container.style.backgroundColor = "green"
     }
 }
 
 function eraser() {
-    gridItem.forEach(item => item.addEventListener("mouseover", ()=> {
+    gridItem.forEach(item => item.addEventListener("mouseover", () => {
         item.style.backgroundColor = `${container.style.backgroundColor}`
     }))
 }
@@ -92,18 +105,16 @@ function clearAll() {
     gridItem.forEach(item => {
         item.style.backgroundColor = "";
     })
-    
+
 }
 
-gridBorder.addEventListener("click", ()=>{
-    gridItem.forEach(item => item.style.border= "1px solid gray");
-})
 
-function setDefault(){
+
+function setDefault() {
     btn.value = 16;
     output.value = btn.value + " x " + btn.value;
 }
-document.addEventListener("DOMContentLoaded", () =>{
+document.addEventListener("DOMContentLoaded", () => {
     setDefault();
     colors();
 })
